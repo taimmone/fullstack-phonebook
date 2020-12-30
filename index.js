378e48b1fb19b3cf36ca8434764f4ea383befd60
Toni Immonen <toni.immonen@tuni.fi>
2020-12-30 11:19:21 +0200

    Implement simple logging (3.7-3.8)

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
 app.use(express.json());
+
+morgan.token('person', (req, res) => JSON.stringify(req.body));
+app.use(morgan('tiny', { skip: (req, res) => req.method === 'POST' }));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :person', {
+    skip: (req, res) => req.method !== 'POST',
+  })
+);
 
 let persons = [
   {
