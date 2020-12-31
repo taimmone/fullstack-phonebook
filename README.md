96c49b81ad39ea752fa29b97c5d961d6467939fc
Toni Immonen <toni.immonen@tuni.fi>
2020-12-31 17:50:14 +0200

    3.11: Fullstack phonebook

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Full Stack Open 2020 Phonebook
 
+[Link to online application](https://shrouded-sea-12695.herokuapp.com/)
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 
@@ -78,5 +79,5 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
