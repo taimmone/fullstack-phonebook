c710228cd4bdf9e90b76b884cd3a90afe05facd0
Toni Immonen <toni.immonen@tuni.fi>
2021-01-01 13:22:52 +0200

    3.15-3.16: Deleted entries reflected in database, error handling

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
-
-app.use(express.static('build'));
-app.use(cors());
 app.use(express.json());
 
-const Person = require('./models/person');
+const cors = require('cors');
+app.use(cors());
 
+const morgan = require('morgan');
 morgan.token('person', (req, res) => JSON.stringify(req.body));
 app.use(morgan('tiny', { skip: (req, res) => req.method === 'POST' }));
 app.use(
@@ -18,34 +16,52 @@ app.use(
   })
 );
 
-app.get('/info', (request, response) => {
-  const info = `Phonebook has info for ${persons.length} people`;
-  const time = new Date();
-  response.send(`<p>${info}</p><p>${time.toString()}</p>`);
+const Person = require('./models/person');
+app.use(express.static('build'));
+
+app.get('/info', (req, res) => {
+  Person.find({}).then(persons => {
+    const info = `Phonebook has info for ${persons.length} people`;
+    const time = new Date();
+    res.send(`<p>${info}</p><p>${time.toString()}</p>`);
+  });
 });
 
-app.get('/api/persons', (request, response) =>
+app.get('/api/persons', (req, res, next) =>
   Person.find({})
-    .then(persons => response.json(persons))
-    .catch(error => console.log(error.message))
+    .then(persons => res.json(persons))
+    .catch(err => next(err))
 );
 
-app.post('/api/persons', (request, response) => {
-  const { name, number } = request.body;
-  if (!name || !number) return response.status(400).json({ error: 'name or number missing' });
+app.post('/api/persons', (req, res, next) => {
+  const { name, number } = req.body;
+  if (!name || !number) return res.status(400).json({ err: 'name or number missing' });
 
-  new Person({ name, number }).save().then(savedPerson => response.json(savedPerson));
+  new Person({ name, number })
+    .save()
+    .then(savedPerson => res.json(savedPerson))
+    .catch(err => next(err));
 });
 
-app.get('/api/persons/:id', (request, response) =>
-  Person.findById(request.params.id)
-    .then(person => response.json(person))
-    .catch(error => console.log(error.message))
+app.get('/api/persons/:id', (req, res, next) =>
+  Person.findById(req.params.id)
+    .then(person => res.json(person))
+    .catch(err => next(err))
 );
 
-app.delete('/api/persons/:id', (request, response) =>
-  response.status(400).json({ error: 'Deletion not implemented' })
+app.delete('/api/persons/:id', (req, res, next) =>
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => res.status(204).end())
+    .catch(err => next(err))
 );
+
+app.use((req, res) => res.status(404).send({ err: 'unknown endpoint' }));
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  if (err.name === 'CastError') return res.status(400).send({ err: 'malformatted id' });
+  next(err);
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
