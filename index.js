05894d65f52a2a57fb5e72f3407e377b456d8d3c
Toni Immonen <toni.immonen@tuni.fi>
2021-01-01 12:40:34 +0200

    3.13-3.14 Database connection

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
-
-const app = express();
 
 app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
+
+const Person = require('./models/person');
 
 morgan.token('person', (req, res) => JSON.stringify(req.body));
 app.use(morgan('tiny', { skip: (req, res) => req.method === 'POST' }));
@@ -16,68 +18,34 @@ app.use(
   })
 );
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1,
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2,
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3,
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4,
-  },
-];
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World</h1>');
-});
-
 app.get('/info', (request, response) => {
   const info = `Phonebook has info for ${persons.length} people`;
   const time = new Date();
   response.send(`<p>${info}</p><p>${time.toString()}</p>`);
 });
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons);
-});
+app.get('/api/persons', (request, response) =>
+  Person.find({})
+    .then(persons => response.json(persons))
+    .catch(error => console.log(error.message))
+);
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body;
-  if (!body.name || !body.number) {
-    return response.status(400).json({ error: 'name or number missing' });
-  }
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({ error: 'name already exists' });
-  }
+  const { name, number } = request.body;
+  if (!name || !number) return response.status(400).json({ error: 'name or number missing' });
 
-  const person = { ...body, id: Math.floor(Math.random() * Math.floor(100000)) };
-  response.json(person);
+  new Person({ name, number }).save().then(savedPerson => response.json(savedPerson));
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
-  if (!person) return response.status(404).end();
-  response.json(person);
-});
+app.get('/api/persons/:id', (request, response) =>
+  Person.findById(request.params.id)
+    .then(person => response.json(person))
+    .catch(error => console.log(error.message))
+);
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-  response.status(204).end();
-});
+app.delete('/api/persons/:id', (request, response) =>
+  response.status(400).json({ error: 'Deletion not implemented' })
+);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
